d442c3a446d340e9640445a73cd3c346db0fcdfb
Aayush Borkar <aayushb14@gmail.com>
2024-03-29 23:29:54 +0530

    Write helper module for conversion

---
+++ tmx_csv_to_npy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+
+def proc(i):
+    return (i % 8) * 875 + (i >> 3)
+
+
+if len(sys.argv) != 3:
+    print("Usage: python tmx_to_npy.py <input_file> <output_file>")
+    sys.exit(1)
+
+a = np.loadtxt(sys.argv[1], delimiter=",", dtype=int)
+np.save(sys.argv[2], proc(a))

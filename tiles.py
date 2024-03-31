3599df0f5d29a478693f29714a24a80af9104ed2
Aayush Borkar <aayushb14@gmail.com>
2024-03-31 10:37:17 +0530

    Better Walls

--- tiles.py
+++ tiles.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import pygame
 
 
@@ -68,5 +69,22 @@ class Tilemap:
                     tile3 = self.tileset.tiles[self.layer3[i, j]]
                     self.image.blit(tile3, (j * 32, i * 32))
                 if 20 <= i < 89 and 20 <= j < 89 and self.maze[i - 20, j - 20]:
-                    wall = self.tileset.tiles[1329]
-                    self.image.blit(wall, (j * 32, i * 32))
+                    wallv = self.tileset.tiles[5490]
+                    wallh = self.tileset.tiles[6367]
+                    hn = 0
+                    vn = 0
+                    if 21 <= i < 89 and self.maze[i - 21, j - 20]:
+                        hn += 1
+                    if i < 88 and self.maze[i - 19, j - 20]:
+                        hn += 1
+                    if 21 <= j < 89 and self.maze[i - 20, j - 21]:
+                        vn += 1
+                    if j < 88 and self.maze[i - 20, j - 19]:
+                        vn += 1
+                    if hn > vn:
+                        self.image.blit(wallh, (j * 32, i * 32))
+                    elif vn > hn:
+                        self.image.blit(wallv, (j * 32, i * 32))
+                    else:
+                        wall = random.choice([wallh, wallv])
+                        self.image.blit(wall, (j * 32, i * 32))

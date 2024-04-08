08621fa57f40324b448394db8d8204ad5a866efc
Aayush Borkar <aayushb14@gmail.com>
2024-04-08 14:36:16 +0530

    Change character for medium

--- game.py
+++ game.py
@@ -13,9 +13,12 @@ ts1 = Tileset("assets/gen5.png")
 ts21 = Tileset("assets/underwater1.png", size=(16, 16))
 ts22 = Tileset("assets/underwater2.png", size=(16, 16))
 ss = Tileset("assets/sprites.png", size=(16, 16))
+sso = Tileset("assets/spriteso.png", size=(16, 16))
 ts1.load()
 ts21.load()
 ts22.load()
+ss.load()
+sso.load()
 
 
 def font_with_size(size):
@@ -33,8 +36,10 @@ def maze(level):
     global running, ss, map
     player_pos = (20, 20)
     sprite = 0
+    s_off = 0
     m = Maze(level, 70)
     map = None
+    cs = None
     if level == 1 or level == 3:
         map = Tilemap(
             ts1,
@@ -49,6 +54,8 @@ def maze(level):
             4378,
             size=(110, 110),
         )
+        cs = sso
+        s_off = 0
     elif level == 2:
         map = Tilemap(
             ts21,
@@ -63,6 +70,9 @@ def maze(level):
             90,
             size=(110, 110),
         )
+        cs = ss
+        s_off = 1
+    assert cs is not None
     assert map is not None
     map.process_layers()
     pygame.key.set_repeat(150, 150)
@@ -79,7 +89,7 @@ def maze(level):
             ),
         )
         screen.blit(
-            pygame.transform.flip(ss.tiles[sprite], rflip, False), (14 * 32, 9 * 32)
+            pygame.transform.flip(cs.tiles[sprite], rflip, False), (14 * 32, 9 * 32)
         )
         move_offset = None
         sprite1 = 0
@@ -92,27 +102,27 @@ def maze(level):
                     pygame.key.set_repeat(0, 0)
                     return menu()
                 if event.key == pygame.K_UP:
-                    sprite = 6
-                    sprite1 = 15
-                    sprite2 = 24
+                    sprite = 6 + s_off
+                    sprite1 = 15 + s_off
+                    sprite2 = 24 + s_off
                     rflip = False
                     move_offset = (0, -1)
                 if event.key == pygame.K_DOWN:
-                    sprite = 0
-                    sprite1 = 9
-                    sprite2 = 18
+                    sprite = 0 + s_off
+                    sprite1 = 9 + s_off
+                    sprite2 = 18 + s_off
                     rflip = False
                     move_offset = (0, 1)
                 if event.key == pygame.K_LEFT:
-                    sprite = 3
-                    sprite1 = 12
-                    sprite2 = 21
+                    sprite = 3 + s_off
+                    sprite1 = 12 + s_off
+                    sprite2 = 21 + s_off
                     rflip = False
                     move_offset = (-1, 0)
                 if event.key == pygame.K_RIGHT:
-                    sprite = 3
-                    sprite1 = 12
-                    sprite2 = 21
+                    sprite = 3 + s_off
+                    sprite1 = 12 + s_off
+                    sprite2 = 21 + s_off
                     rflip = True
                     move_offset = (1, 0)
         if (
@@ -139,12 +149,12 @@ def maze(level):
                 )
                 if i % 8:
                     screen.blit(
-                        pygame.transform.flip(ss.tiles[sprite1], rflip, False),
+                        pygame.transform.flip(cs.tiles[sprite1], rflip, False),
                         (14 * 32, 9 * 32),
                     )
                 else:
                     screen.blit(
-                        pygame.transform.flip(ss.tiles[sprite2], rflip, False),
+                        pygame.transform.flip(cs.tiles[sprite2], rflip, False),
                         (14 * 32, 9 * 32),
                     )
                 pygame.display.update()
